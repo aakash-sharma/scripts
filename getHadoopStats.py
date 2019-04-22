e29655be32370c453fc2ce267b1f7ad896ea5442
Aakash Sharma <asharma.com@gmail.com>
2019-04-22 12:47:48 -0400

    Added support to parse log files

--- getHadoopStats.py
+++ getHadoopStats.py
@@ -1,3 +1,4 @@
+import sys
 import csv 
 import wget
 import os
@@ -5,6 +6,9 @@ import json
 import xlwt
 import xlrd
 from pprint import pprint
+from os import listdir
+from os.path import isfile, join
+import datetime
 
 
 URI='http://0.0.0.0:19888/ws/v1/history/'
@@ -32,25 +36,23 @@ JobProperties = ('name',
                  'map_output_bytes',
                  'reduce_shuffle_bytes',
                  'gc_time_millis',
-                 'cpu_milliseconds')
+                 'cpu_milliseconds',
+                 'slots_millis_maps',
+                 'slots_millis_reduce',
+                 'vcores_millis_maps',
+                 'vcores_millis_reduce',
+                 'millis_maps',
+                 'millis_reduce')
 
-class Job:
-    def __init__(self, jobProperties):
-        name = jobProperties[0]
-        id = jobProperties[1]
-        startTime = jobProperties[2]
-        finishTime = jobProperties[3]
-        avgMapTime = jobProperties[4]
-        avgReduceTime = jobProperties[5]
-        avgShuffleTime = jobProperties[6]
-        avgMergeTime = jobProperties[7]
-        
 
 def checkFileExists(fileName):
     exists = os.path.isfile(fileName)
 
     if exists:
-        os.remove(fileName)
+        return True
+    else:
+        return False
+        #os.remove(fileName)
 
 def getStartTime():
     checkFileExists('historyServer.json')
@@ -60,13 +62,15 @@ def getStartTime():
     with open('historyServer.json') as fd:
         webPage = json.load(fd)
 
+    print(webPage['historyInfo']['startedOn'])
     return (webPage['historyInfo']['startedOn'])
 
 def getJobs(jobs): 
   
-    checkFileExists('jobs.json')
+    exists = checkFileExists('jobs.json')
 
-    wget.download(URI + 'mapreduce/jobs/', 'jobs.json')
+    if not exists:
+        wget.download(URI + 'mapreduce/jobs/', 'jobs.json')
 
     with open('jobs.json') as fd:
         webPage = json.load(fd)
@@ -78,13 +82,12 @@ def getJobProperties(jobId, jobProperties):
 
     uri = URI + 'mapreduce/jobs/' + jobId
 
-    print(uri)
-
     fileName = jobId + '.json'
     
-    checkFileExists(fileName)
+    exists = checkFileExists(fileName)
     
-    wget.download(uri, fileName)
+    if not exists:
+        wget.download(uri, fileName)
 
     with open(fileName) as fd:
         webPage = json.load(fd)
@@ -103,13 +106,12 @@ def getJobCounters(jobId, jobProperties):
 
     uri = URI + 'mapreduce/jobs/' + jobId + '/counters'
 
-    print(uri)
-
     fileName = jobId + '_counters.json'
 
-    checkFileExists(fileName)
+    exists = checkFileExists(fileName)
 
-    wget.download(uri, fileName)
+    if not exists:
+        wget.download(uri, fileName)
 
     jobCounters = []
 
@@ -165,17 +167,49 @@ def getJobCounters(jobId, jobProperties):
                     jobProperties[JobProperties.index('cpu_milliseconds')] = webPage['jobCounters']['counterGroup'][i]['counter'][j]['totalCounterValue']
 
 
-#    jobProperties.append(webPage['job'][JobProperties[0]])
-#    jobProperties.append(webPage['job'][JobProperties[1]])
-#    jobProperties.append(webPage['job'][JobProperties[2]])
-#    jobProperties.append(webPage['job'][JobProperties[3]])
-#    jobProperties.append(webPage['job'][JobProperties[4]])
-#    jobProperties.append(webPage['job'][JobProperties[5]])
-#    jobProperties.append(webPage['job'][JobProperties[6]])
-#    jobProperties.append(webPage['job'][JobProperties[7]])
+def getJobRMCounter(job, path, jobProperties):
+   
+    exists = os.path.isdir(path)
 
-#    return (jobProperties)
+    if not exists:
+        return
+    
+    files = [join(path,f) for f in listdir(path) if isfile(join(path, f))]
+    flag = 1
 
+    for f in files:
+        if flag == 0:
+            break
+        with open(f, 'r') as logFile:
+            line = logFile.readline()
+            while line:
+                if "Job " + job + " completed successfully" in line:
+                    for i in range(62):
+                        line = logFile.readline()
+                        
+                        if "Total time spent by all maps in occupied slots" in line:
+                            jobProperties[JobProperties.index('slots_millis_maps')] = int(line.split('=')[1])
+                
+                        if "Total time spent by all reduces in occupied slots" in line:
+                            jobProperties[JobProperties.index('slots_millis_reduce')] = int(line.split('=')[1])
+                        
+                        if "Total time spent by all map tasks" in line:
+                            jobProperties[JobProperties.index('millis_maps')] = int(line.split('=')[1])
+                        
+                        if "Total time spent by all reduce tasks" in line:
+                            jobProperties[JobProperties.index('millis_reduce')] = int(line.split('=')[1])
+                        
+                        if "Total vcore-milliseconds taken by all map tasks" in line:
+                            jobProperties[JobProperties.index('vcores_millis_maps')] = int(line.split('=')[1])
+                        
+                        if "Total vcore-milliseconds taken by all reduce tasks" in line:
+                            jobProperties[JobProperties.index('vcores_millis_reduce')] = int(line.split('=')[1])
+
+                    flag = 0
+                    break
+                line = logFile.readline()
+            logFile.close()
+                
 
 def savetoCSV(jobResults): 
  
@@ -205,30 +239,101 @@ def is_number(s):
 
 def saveToXLS(jobResults):
 
+    CleanJobProps = (   'name',
+                        'makespan',
+                        'B_read',
+                        'B_write',
+                        'F_B_read',
+                        'F_B_write',
+                        'H_B_read',
+                        'H_B_write',
+                        'M_out_bytes',
+                        'R_shuffle_bytes',
+                        'CPU_time',
+                        'AvgMapTime',
+                        'AvgRdcTime',
+                        'AvgShuffleTime',
+                        'AvgMgTime',
+                        'millis_maps',
+                        'millis_reduce'
+                        'Non CPU Time'
+                        'CPU Utilization')
+                    
     row_list = []
+    row_list2 = []
+    row2 = []
     style = xlwt.XFStyle()
     style.num_format_str = '#,###0.00'
 
     row_list.append(JobProperties)
+    row_list2.append(CleanJobProps)
 
     for row in jobResults:
+
+        for i in range(len(row)):
+            if row[i] == None:
+                row[i] = 0
+        
         row_list.append(row)
+
+        row2 = []
+
+        row2.append(row[JobProperties.index('name')])
+        row2.append(row[JobProperties.index('finishTime')]-row[JobProperties.index('startTime')])
+        row2.append(row[JobProperties.index('bytes_read_map')] + row[JobProperties.index('bytes_read_reduce')])
+        row2.append(row[JobProperties.index('bytes_written_map')] + row[JobProperties.index('bytes_written_reduce')])
+        row2.append(row[JobProperties.index('file_bytes_read_map')] + row[JobProperties.index('file_bytes_read_reduce')])
+        row2.append(row[JobProperties.index('file_bytes_written_map')] + row[JobProperties.index('file_bytes_written_reduce')])
+        row2.append(row[JobProperties.index('hdfs_bytes_read_map')] + row[JobProperties.index('hdfs_bytes_read_reduce')])
+        row2.append(row[JobProperties.index('hdfs_bytes_written_map')] + row[JobProperties.index('hdfs_bytes_written_reduce')])
+        row2.append(row[JobProperties.index('map_output_bytes')])
+        row2.append(row[JobProperties.index('reduce_shuffle_bytes')])
+        row2.append(row[JobProperties.index('cpu_milliseconds')] + row[JobProperties.index('gc_time_millis')])
+        row2.append(row[JobProperties.index('avgMapTime')])
+        row2.append(row[JobProperties.index('avgReduceTime')])
+        row2.append(row[JobProperties.index('avgShuffleTime')])
+        row2.append(row[JobProperties.index('avgMergeTime')])
+        row2.append(row[JobProperties.index('millis_maps')])
+        row2.append(row[JobProperties.index('millis_reduce')])
+        row2.append(row[JobProperties.index('millis_reduce')] + row[JobProperties.index('millis_reduce')] - row[JobProperties.index('cpu_milliseconds')] - row[JobProperties.index('gc_time_millis')])
+        if (row[JobProperties.index('vcores_millis_maps')] + row[JobProperties.index('vcores_millis_reduce')]) == 0:
+            row2.append(-1)
+        else:
+            row2.append(row[JobProperties.index('cpu_milliseconds')] / (row[JobProperties.index('vcores_millis_maps')] + row[JobProperties.index('vcores_millis_reduce')]) * 100)
+
+        row_list2.append(row2)
  
     column_list = zip(*row_list)
     workbook = xlwt.Workbook()
-    worksheet = workbook.add_sheet('Sheet1')
+    worksheet1 = workbook.add_sheet('Raw data')
     i = 0
+
     for column in column_list:
         for item in range(len(column)):
             value = column[item]
             if value == None:
                 value = 0
             if is_number(value):
-                worksheet.write(item, i, float(value), style=style)
+                worksheet1.write(item, i, value, style=style)
             else:
-                worksheet.write(item, i, value)
+                worksheet1.write(item, i, value)
         i+=1
      
+    column_list2 = zip(*row_list2)
+    worksheet2 = workbook.add_sheet('Data')
+
+    i = 0
+    for column in column_list2:
+        for item in range(len(column)):
+            value = column[item]
+            if value == None:
+                value = 0
+            if is_number(value):
+                worksheet2.write(item, i, value, style=style)
+            else:
+                worksheet2.write(item, i, value)
+        i+=1
+
     workbook.save('report.xls')
  
 def main(): 
@@ -236,30 +341,32 @@ def main():
     jobs = []
     jobProperties = [None] * len(JobProperties)
     jobResults = []
-   
-    startedOn = getStartTime()
-    
+  
+    if len(sys.argv) < 1:
+        return
+
+    startedOn = sys.argv[1]
+
     path = os.getcwd()
-    workDir = path + os.path.sep + str(startedOn)
+    workDir = "/home/abs5688/cloudlab/results" + os.path.sep + str(startedOn)
     exists = os.path.isdir(workDir)
     if not exists:
         os.mkdir(workDir)
+    else:
+        print ("Using pre existing data",  startedOn )
     os.chdir(workDir)
     
     getJobs(jobs) 
     
-    pprint(jobs)
+   # pprint(jobs)
     for job in jobs:
         getJobProperties(job, jobProperties)
         getJobCounters(job, jobProperties)
+        getJobRMCounter(job, workDir + os.path.sep + "result-" + str(startedOn), jobProperties)
         jobResults.append(jobProperties.copy())
+        jobProperties = [None] * len(JobProperties)
 
     saveToXLS(jobResults)
-    #for property in jobProperties:
-    #    jobs.append(Job(property))
-    #pprint()
-    pprint(jobResults)
-    print(JobProperties)
 
 if __name__ == "__main__": 
   
