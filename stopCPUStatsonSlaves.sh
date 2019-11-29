65abf1711565cc8745812d5ab87a036aca6ff915
aakashsh <asharma.com@gmail.com>
2019-11-28 21:24:47 -0500

    New changes

--- stopCPUStatsonSlaves.sh
+++ stopCPUStatsonSlaves.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 range=$1
+workload=$2
 i=0
+DATE=`date +%F-%T`
+
+mkdir ~/cpu_stats/${workload}_${DATE}
+cp ~/cpu_stats/cpu_stats.slave* ~/cpu_stats/${workload}_${DATE}
 
 while [ $i -lt $range ];
 do
