9179bee91a44fdd964d5d27581a5940fc828a26f
Aakash Sharma <asharma.com@gmail.com>
2021-03-04 23:24:16 -0500

    Improved visualizations

--- compareTezCounters.py
+++ compareTezCounters.py
@@ -15,54 +15,157 @@ from scipy.stats.mstats import gmean
 import matplotlib.pyplot as plt
 from getTezCountersFromTimeline import *
 
-def plotGraph(filteredDagResults, filteredVertexResults):
-    fig, axs = plt.subplots(2, figsize=(15,15))
+START_SIZE = 50
+
+def plotGraphDag(filteredDagResults):
+    fig, axs = plt.subplots(2, figsize=(12,6))
     n = len(filteredDagResults)
     dag_len = 0
     
-    for i in range(n):
-        max_dag_len = max(dag_len, len(filteredDagResults[i]))
+    dag_len = len(filteredDagResults[0])
     
-    x_axis = [i for i in range(max_dag_len)]
+    x_axis = [i for i in range(dag_len)]
     y_axis_corr_cpu_hdfs_data_map = [] 
     y_axis_corr_cpu_hdfs_data_reduce = []
     y_axis_corr_spillage_hdfs_data_map = []
     y_axis_corr_spillage_hdfs_data_reduce = []
+    y_axis_cpu_util_dag = []
+    y_axis_spillage_dag = []
 
     for i in range(n):
 
-        dag_len = len(filteredDagResults[i])
+#        dag_len = len(filteredDagResults[i])
         y_axis_corr_cpu_hdfs_data_map.append([])
         y_axis_corr_cpu_hdfs_data_reduce.append([])
         y_axis_corr_spillage_hdfs_data_map.append([])
         y_axis_corr_spillage_hdfs_data_reduce.append([])
 
         for j in range(dag_len):
-            #print(y_axis_corr_cpu_hdfs_data_map)
-            #print(y_axis_corr_cpu_hdfs_data_map[i])
-            #print(filteredDagResults[i])
-            #print(filteredDagResults[i][j])
-            #print(filteredDagResults[i][j][FilteredDagProperties.index('corr_cpu_hdfs_data_map')])
             y_axis_corr_cpu_hdfs_data_map[i].append(filteredDagResults[i][j][FilteredDagProperties.index('corr_cpu_local_data_map')])
             y_axis_corr_cpu_hdfs_data_reduce[i].append(filteredDagResults[i][j][FilteredDagProperties.index('corr_cpu_shuffle_data_reduce')])
 
             y_axis_corr_spillage_hdfs_data_map[i].append(filteredDagResults[i][j][FilteredDagProperties.index('corr_spillage_local_data_map')])
             y_axis_corr_spillage_hdfs_data_reduce[i].append(filteredDagResults[i][j][FilteredDagProperties.index('corr_spillage_shuffle_data_reduce')])
 
+    label = START_SIZE
     for i in range(n):
-        axs[0].plot(x_axis, y_axis_corr_cpu_hdfs_data_map[i])
-        axs[0].plot(x_axis, y_axis_corr_spillage_hdfs_data_map[i])
+        axs[0].plot(x_axis, y_axis_corr_cpu_hdfs_data_map[i], label=("cpu " + str(label)+" GB"))
+        label *= 2
 
-    axs[0].set_ylabel('Correlation')
+    label = START_SIZE
+    for i in range(n):
+        axs[0].plot(x_axis, y_axis_corr_spillage_hdfs_data_map[i], label=("spillage " + str(label)+" GB"))
+        label *= 2
+
+    label = START_SIZE
+    for i in range(n):
+        axs[1].plot(x_axis, y_axis_corr_cpu_hdfs_data_reduce[i], label=("cpu " + str(label)+" GB"))
+        label *= 2
+
+    label = START_SIZE
+    for i in range(n):
+        axs[1].plot(x_axis, y_axis_corr_spillage_hdfs_data_reduce[i], label=("spillage " + str(label)+" GB"))
+        label *= 2
+
+    axs[0].set_ylabel('Correlation - Map')
     axs[0].set_xlabel('Queries')
+    box = axs[0].get_position()
+    axs[0].set_position([box.x0, box.y0 + box.height * 0.1, box.width, box.height * 0.9])
+    axs[0].legend(loc="upper center", bbox_to_anchor=(0.5, -0.05),fancybox=True, shadow=True, ncol=5)
+
+    axs[1].set_ylabel('Correlation - Reduce')
+    axs[1].set_xlabel('Queries')
+    box = axs[1].get_position()
+    axs[1].set_position([box.x0, box.y0 + box.height * 0.1, box.width, box.height * 0.9])
+    axs[1].legend(loc="upper center", bbox_to_anchor=(0.5, -0.05),fancybox=True, shadow=True, ncol=5)
+#    axs[1].legend(loc="best")
+            
+    plt.show()    
+
+def plotGraphVertex(filteredVertexResults):
+    fig, axs = plt.subplots(2, 2, figsize=(10,10))
+    n = len(filteredVertexResults)
+    
+    vertices_len = max([len(filteredVertexResults[i]) for i in range(n)])
+    
+    x_axis = [i for i in range(vertices_len)]
+    y_axis_cpu_util_map = [] 
+    y_axis_cpu_util_reduce = [] 
+    y_axis_spillage_map = []
+    y_axis_spillage_reduce = []
 
     for i in range(n):
-        axs[1].plot(x_axis, y_axis_corr_cpu_hdfs_data_reduce[i])
-        axs[1].plot(x_axis, y_axis_corr_spillage_hdfs_data_reduce[i])
 
-    axs[1].set_ylabel('Correlation')
-    axs[1].set_xlabel('Queries')
-            
+#        dag_len = len(filteredDagResults[i])
+        y_axis_cpu_util_map.append([])
+        y_axis_cpu_util_reduce.append([])
+        y_axis_spillage_map.append([])
+        y_axis_spillage_reduce.append([])
+        
+        Map = 0
+        Reduce = 0
+
+        for j in range(len(filteredVertexResults[i])):
+            if "Map" in filteredVertexResults[i][j][FilteredVertexProperties.index('vertexName')]:
+                Map += 1
+                y_axis_cpu_util_map[i].append(filteredVertexResults[i][j][FilteredVertexProperties.index('avgTaskCPUutil')])
+                y_axis_spillage_map[i].append(filteredVertexResults[i][j][FilteredVertexProperties.index('avgTaskSpilledRecordsPerSec')])
+
+            if "Reduce" in filteredVertexResults[i][j][FilteredVertexProperties.index('vertexName')]:
+                Reduce += 1
+                y_axis_cpu_util_reduce[i].append(filteredVertexResults[i][j][FilteredVertexProperties.index('avgTaskCPUutil')])
+
+                y_axis_spillage_reduce[i].append(filteredVertexResults[i][j][FilteredVertexProperties.index('avgTaskSpilledRecordsPerSec')])
+
+    #    print(Map, Reduce)
+
+    label = START_SIZE
+    for i in range(n):
+        
+        x_axis = [i for i in range(len(y_axis_cpu_util_map[i]))]
+        axs[0, 0].plot(x_axis, y_axis_cpu_util_map[i], label=(str(label)+" GB"))
+        
+        x_axis = [i for i in range(len(y_axis_spillage_map[i]))]
+        axs[1, 0].plot(x_axis, y_axis_spillage_map[i], label=(str(label)+" GB"))
+
+        x_axis = [i for i in range(len(y_axis_cpu_util_reduce[i]))]
+        axs[0, 1].plot(x_axis, y_axis_cpu_util_reduce[i], label=(str(label)+" GB"))
+
+        x_axis = [i for i in range(len(y_axis_spillage_reduce[i]))]
+        axs[1, 1].plot(x_axis, y_axis_spillage_reduce[i], label=(str(label)+" GB"))
+
+        label *= 2
+
+    axs[0, 0].set_ylabel('CPU Utilization - Map')
+    axs[0, 0].set_xlabel('Vertices')
+    axs[0, 0].legend(loc="upper left")
+    
+    axs[0, 1].set_ylabel('CPU Utilization - Reduce')
+    axs[0, 1].set_xlabel('Vertices')
+    axs[0, 1].legend(loc="upper left")
+
+    axs[1, 0].set_ylabel('Spillage - Map')
+    axs[1, 0].set_xlabel('Vertices')
+    axs[1, 0].legend(loc="upper left")
+
+    axs[1, 1].set_ylabel('Spillage - Reduce')
+    axs[1, 1].set_xlabel('Vertices')
+    axs[1, 1].legend(loc="upper left")
+
+
+    """ 
+    label = 100
+    for i in range(n):
+        x_axis = [i for i in range(len(y_axis_cpu_util_reduce[i]))]
+        axs[0, 1].plot(x_axis, y_axis_cpu_util_reduce[i], label=(str(label*(i+1))+" GB"))
+
+    axs[0, 1].set_ylabel('CPU Utilization')
+    axs[0, 1].set_xlabel('Vertices')
+    axs[0, 1].legend(loc="upper left")
+    """
+
+
+        
     plt.show()    
 
 def cleanDags(dagResults):
@@ -80,7 +183,7 @@ def cleanDags(dagResults):
         i = 0
         while i < len(exp):
             if exp[i][DagProperties.index('dagDescHash')] not in dagHash:
-                print(exp[i][DagProperties.index('dagDescHash')])
+                #print(exp[i][DagProperties.index('dagDescHash')])
                 del exp[i]
             else:
                 i += 1
@@ -117,7 +220,8 @@ def main():
         os.chdir("../")
 
 #    saveToXLS(dagResults, vertexResults, filteredDagResults, filteredVertexResults, startedOn)
-    plotGraph(filteredDagResults, filteredVertexResults)
+    plotGraphDag(filteredDagResults)
+    plotGraphVertex(filteredVertexResults)
 
 if __name__ == "__main__":
     main()
