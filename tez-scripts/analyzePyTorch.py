404940a989eda61522fcb6d1f42736c93aa57d6d
Aakash Sharma <asharma.com@gmail.com>
2021-05-15 22:58:20 -0400

    initial commit

---
+++ tez-scripts/analyzePyTorch.py
@@ -0,0 +1,233 @@
+import sys
+import os
+from os import listdir
+import datetime
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy import vstack
+from numpy import sqrt
+from sklearn.metrics import mean_squared_error
+import torch
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+from torch.utils.data import random_split
+from torch import nn
+from torch import Tensor
+from torch.nn import Linear
+from torch.nn import ReLU
+from torch.nn import Sigmoid
+from torch.nn import Module
+from torch.optim import SGD
+from torch.nn import MSELoss
+from torch.nn import CrossEntropyLoss
+from torch.nn.init import xavier_uniform_
+from torchvision import transforms
+from TezCountersBase import TezCountersBase, VertexProperties
+
+NUM_INPUT = 5
+NUM_OUTPUT = 2
+
+
+class TezCountersData(Dataset):
+
+    def __init__(self, workDirs):
+        self.exps = []
+        self.cwd = os.getcwd()
+        self.vertexData = []
+
+        for wd in workDirs:
+            tez = TezCountersBase(wd)
+            for vertex in tez.vertexResults:
+                result = []
+                result.append(self.codeVertexManager(vertex[VertexProperties.index('vertexManagerName')]))
+                result.append(vertex[VertexProperties.index('runTime')])
+                #result.append(vertex[VertexProperties.index('HDFS_BYTES_READ')])
+                #result.append(vertex[VertexProperties.index('HDFS_BYTES_WRITTEN')])
+                result.append(vertex[VertexProperties.index('FILE_BYTES_READ')])
+                result.append(vertex[VertexProperties.index('FILE_BYTES_WRITTEN')])
+                result.append(vertex[VertexProperties.index('SHUFFLE_BYTES')])
+                result.append(vertex[VertexProperties.index('avgTaskCPUutil')])
+                result.append(vertex[VertexProperties.index('SPILLED_RECORDS')])
+                self.vertexData.append(result.copy())
+
+            del tez
+            os.chdir(self.cwd)
+
+        self.vertexData = np.array(self.vertexData)
+        
+        self.X = self.vertexData[:,1:NUM_INPUT].astype('float32')
+        self.normalize(self.X)
+        self.X = np.append(self.X, self.vertexData[:,:1].astype('float32'), axis=1)
+
+        self.Y = self.vertexData[:,NUM_INPUT:].astype('float32')
+        self.normalize(self.Y)
+        self.Y = self.Y.reshape((len(self.Y), 2))
+
+
+#        transform = transforms.Compose([
+#          transforms.ToTensor(),
+#        transforms.Normalize(mean=[.5],
+#                         std=[1])
+#        ])
+
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, idx):
+        return [self.X[idx], self.Y[idx]]
+
+    def get_splits(self, n_test=0.33):
+        # determine sizes
+        test_size = round(n_test * len(self.X))
+        train_size = len(self.X) - test_size
+        # calculate the split
+        return random_split(self, [train_size, test_size])
+
+
+    def codeVertexManager(self, manager):
+        if manager == "RootInputVertexManager":
+            return 0
+
+        if manager == "ShuffleVertexManager":
+            return 1
+
+    def normalize(self, data):
+        means = np.mean(data, axis = 0)
+        stds = np.var(data, axis = 0)
+        data = (data-means)/stds
+
+
+class NNmodel(nn.Module):
+    def __init__(self, n_inputs):
+        super(NNmodel, self).__init__()
+
+        self.hidden1 = Linear(n_inputs, NUM_INPUT*NUM_OUTPUT*NUM_INPUT)
+        #xavier_uniform_(self.hidden1.weight)
+        #self.act_rel1 = ReLU()
+        self.act_rel1 = Sigmoid()
+        self.hidden2 = Linear(NUM_OUTPUT*NUM_INPUT*NUM_INPUT, NUM_INPUT)
+        #xavier_uniform_(self.hidden2.weight)
+        self.act_rel2 = ReLU()
+#        self.act_rel2 = Sigmoid()
+        self.hidden3 = Linear(NUM_INPUT, NUM_OUTPUT)
+        #self.hidden3 = Linear(NUM_INPUT, 1)
+        #xavier_uniform_(self.hidden3.weight)
+        #self.act_sig = Sigmoid()
+
+    def forward(self, X):
+        X = self.hidden1(X)
+        X = self.act_rel1(X)
+        X = self.hidden2(X)
+        X = self.act_rel2(X)
+        X = self.hidden3(X)
+
+        return X
+                
+# prepare the dataset
+def prepare_data(workDirs):
+    # load the dataset
+    dataset = TezCountersData(workDirs)
+    # calculate split
+    train, test = dataset.get_splits()
+    # prepare data loaders
+    train_dl = DataLoader(train, batch_size=len(train), shuffle=True)
+    test_dl = DataLoader(test, batch_size=32, shuffle=False)
+    return train_dl, test_dl
+
+# train the model
+def train_model(train_dl, model):
+    # define the optimization
+    criterion = MSELoss()
+#    criterion = CrossEntropyLoss()
+    optimizer = SGD(model.parameters(), lr=0.01, momentum=0.9)
+    # enumerate epochs
+    for epoch in range(20):
+        print('Training epoch ', epoch)
+        # enumerate mini batches
+        for i, (inputs, targets) in enumerate(train_dl):
+            # clear the gradients
+            optimizer.zero_grad()
+            #print('input: ', inputs)
+            # compute the model output
+            yhat = model(inputs)
+#            print('model output: ', yhat)
+            # calculate loss
+            loss = criterion(yhat, targets)
+            # credit assignment
+            loss.backward()
+            # update model weights
+            optimizer.step()
+            #print('target: ', targets)
+            print('batch ', i, ', loss: ', loss)
+        print(yhat)
+    print('Finished training')
+
+# evaluate the model
+def evaluate_model(test_dl, model):
+    predictions, actuals = list(), list()
+    for i, (inputs, targets) in enumerate(test_dl):
+        # evaluate the model on the test set
+        print('input: ', inputs)
+        yhat = model(inputs)
+        #print(yhat)
+        print('model op: ', yhat)
+        # retrieve numpy array
+        yhat = yhat.detach().numpy()
+        actual = targets.numpy()
+        print('target: ', actual)
+        #actual = actual.reshape((len(actual), 1))
+        # round to class values
+        #yhat = yhat.round()
+        # store
+        predictions.append(yhat)
+        actuals.append(actual) 
+        break
+    predictions, actuals = vstack(predictions), vstack(actuals)
+    # calculate accuracy
+    #print(actuals)
+    #print(predictions)
+    mse = mean_squared_error(actuals, predictions)
+    return mse
+
+# make a class prediction for one row of data
+def predict(row, model):
+    # convert row to data
+    row = Tensor([row])
+    # make prediction
+    yhat = model(row)
+    # retrieve numpy array
+    yhat = yhat.detach().numpy()
+    return yhat
+
+def main():
+
+    workDirs = []
+
+    if len(sys.argv) < 2:
+        print("Please provide at least two dirs")
+        return 
+
+    for i in range(1, len(sys.argv)):
+        workDirs.append(sys.argv[i])
+
+    train_dl, test_dl = prepare_data(workDirs)
+    print(len(train_dl.dataset), len(test_dl.dataset))
+
+    model = NNmodel(NUM_INPUT)
+    print(model)
+    #return
+    train_model(train_dl, model)
+    # evaluate the model
+    #mse = evaluate_model(test_dl, model)
+    #model.eval()
+#    mse = evaluate_model(train_dl, model)
+#    print('MSE: %.3f, RMSE: %.3f' % (mse, sqrt(mse)))
+#    row = [0, 7641, 104457151, 0, 8672, 130778, 0]
+ #   yhat = predict(row, model)
+ #   print(yhat)
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ tez-scripts/compareTezCounters.py
@@ -0,0 +1,358 @@
+import sys
+import csv
+import wget
+import os
+import json
+import xlwt
+import xlrd
+from pprint import pprint
+from os import listdir
+from os.path import isfile, join
+import datetime
+from shutil import copy
+from scipy.stats import pearsonr
+from scipy.stats import variation
+from scipy.stats.mstats import gmean 
+import matplotlib.pyplot as plt
+from getTezCountersFromTimeline import *
+
+START_SIZE = 50
+
+
+def plotGraphDag(filteredDagResults):
+    fig, axs = plt.subplots(2, figsize=(12,6))
+    n = len(filteredDagResults)
+    dag_len = len(filteredDagResults[0])
+    
+    x_axis = [i for i in range(dag_len)]
+    y_axis_corr_cpu_hdfs_data_map = [] 
+    y_axis_corr_cpu_hdfs_data_reduce = []
+    y_axis_corr_spillage_hdfs_data_map = []
+    y_axis_corr_spillage_hdfs_data_reduce = []
+    y_axis_cpu_util_dag = []
+    y_axis_spillage_dag = []
+
+    for i in range(n):
+
+#        dag_len = len(filteredDagResults[i])
+        y_axis_corr_cpu_hdfs_data_map.append([])
+        y_axis_corr_cpu_hdfs_data_reduce.append([])
+        y_axis_corr_spillage_hdfs_data_map.append([])
+        y_axis_corr_spillage_hdfs_data_reduce.append([])
+
+        for j in range(dag_len):
+            y_axis_corr_cpu_hdfs_data_map[i].append(filteredDagResults[i][j][FilteredDagProperties.index('corr_cpu_local_data_map')])
+            y_axis_corr_cpu_hdfs_data_reduce[i].append(filteredDagResults[i][j][FilteredDagProperties.index('corr_cpu_shuffle_data_reduce')])
+
+            y_axis_corr_spillage_hdfs_data_map[i].append(filteredDagResults[i][j][FilteredDagProperties.index('corr_spillage_local_data_map')])
+            y_axis_corr_spillage_hdfs_data_reduce[i].append(filteredDagResults[i][j][FilteredDagProperties.index('corr_spillage_shuffle_data_reduce')])
+
+    label = START_SIZE
+    for i in range(n):
+        axs[0].plot(x_axis, y_axis_corr_cpu_hdfs_data_map[i], label=("cpu " + str(label)+" GB"))
+        label *= 2
+
+    label = START_SIZE
+    for i in range(n):
+        axs[0].plot(x_axis, y_axis_corr_spillage_hdfs_data_map[i], label=("spillage " + str(label)+" GB"))
+        label *= 2
+
+    label = START_SIZE
+    for i in range(n):
+        axs[1].plot(x_axis, y_axis_corr_cpu_hdfs_data_reduce[i], label=("cpu " + str(label)+" GB"))
+        label *= 2
+
+    label = START_SIZE
+    for i in range(n):
+        axs[1].plot(x_axis, y_axis_corr_spillage_hdfs_data_reduce[i], label=("spillage " + str(label)+" GB"))
+        label *= 2
+
+    axs[0].set_ylabel('Correlation - Map')
+    axs[0].set_xlabel('Queries')
+    box = axs[0].get_position()
+    axs[0].set_position([box.x0, box.y0 + box.height * 0.1, box.width, box.height * 0.9])
+    axs[0].legend(loc="upper center", bbox_to_anchor=(0.5, -0.05),fancybox=True, shadow=True, ncol=5)
+
+    axs[1].set_ylabel('Correlation - Reduce')
+    axs[1].set_xlabel('Queries')
+    box = axs[1].get_position()
+    axs[1].set_position([box.x0, box.y0 + box.height * 0.1, box.width, box.height * 0.9])
+    axs[1].legend(loc="upper center", bbox_to_anchor=(0.5, -0.05),fancybox=True, shadow=True, ncol=5)
+#    axs[1].legend(loc="best")
+            
+    plt.show()    
+
+def plotGraphVertex(vertexResults):
+    fig, axs = plt.subplots(2, 2, figsize=(10,10))
+    n = len(vertexResults)
+    
+    vertices_len = max([len(vertexResults[i]) for i in range(n)])
+    
+    x_axis = [i for i in range(vertices_len)]
+    y_axis_cpu_util_map = [] 
+    y_axis_cpu_util_reduce = [] 
+    y_axis_spillage_map = []
+    y_axis_spillage_reduce = []
+    y_axis_num_tasks_map = []
+    y_axis_num_tasks_reduce = []
+
+    for i in range(n):
+
+#        dag_len = len(filteredDagResults[i])
+        y_axis_cpu_util_map.append([])
+        y_axis_cpu_util_reduce.append([])
+        y_axis_spillage_map.append([])
+        y_axis_spillage_reduce.append([])
+        y_axis_num_tasks_map.append([])
+        y_axis_num_tasks_reduce.append([])
+        
+        Map = 0
+        Reduce = 0
+
+        for j in range(len(vertexResults[i])):
+            if "Map" in vertexResults[i][j][VertexProperties.index('vertexName')]:
+                Map += 1
+                y_axis_cpu_util_map[i].append(vertexResults[i][j][VertexProperties.index('avgTaskCPUutil')])
+                y_axis_spillage_map[i].append(vertexResults[i][j][VertexProperties.index('avgTaskSpilledRecordsPerSec')])
+                y_axis_num_tasks_map[i].append(vertexResults[i][j][VertexProperties.index('numSucceededTasks')])
+
+            if "Reduce" in vertexResults[i][j][VertexProperties.index('vertexName')]:
+                Reduce += 1
+                y_axis_cpu_util_reduce[i].append(vertexResults[i][j][VertexProperties.index('avgTaskCPUutil')])
+
+                y_axis_spillage_reduce[i].append(vertexResults[i][j][VertexProperties.index('avgTaskSpilledRecordsPerSec')])
+                y_axis_num_tasks_reduce[i].append(vertexResults[i][j][VertexProperties.index('numSucceededTasks')])
+
+    #    print(Map, Reduce)
+
+    label = START_SIZE
+    for i in range(n):
+        
+        x_axis = [i for i in range(len(y_axis_cpu_util_map[i]))]
+        axs[0, 0].plot(x_axis, y_axis_cpu_util_map[i], label=(str(label)+" GB"))
+        
+        x_axis = [i for i in range(len(y_axis_spillage_map[i]))]
+        axs[1, 0].plot(x_axis, y_axis_spillage_map[i], label=(str(label)+" GB"))
+
+        x_axis = [i for i in range(len(y_axis_cpu_util_reduce[i]))]
+        axs[0, 1].plot(x_axis, y_axis_cpu_util_reduce[i], label=(str(label)+" GB"))
+
+        x_axis = [i for i in range(len(y_axis_spillage_reduce[i]))]
+        axs[1, 1].plot(x_axis, y_axis_spillage_reduce[i], label=(str(label)+" GB"))
+
+        label *= 2
+
+    axs[0, 0].set_ylabel('CPU Utilization - Map')
+    axs[0, 0].set_xlabel('Vertices')
+    axs[0, 0].legend(loc="upper left")
+    
+    axs[0, 1].set_ylabel('CPU Utilization - Reduce')
+    axs[0, 1].set_xlabel('Vertices')
+    axs[0, 1].legend(loc="upper left")
+
+    axs[1, 0].set_ylabel('Spillage - Map')
+    axs[1, 0].set_xlabel('Vertices')
+    axs[1, 0].legend(loc="upper left")
+
+    axs[1, 1].set_ylabel('Spillage - Reduce')
+    axs[1, 1].set_xlabel('Vertices')
+    axs[1, 1].legend(loc="upper left")
+
+    plt.show()    
+
+    fig, axs = plt.subplots(2, figsize=(10,10))
+
+    label = START_SIZE
+    for i in range(n):
+        
+        x_axis = [i for i in range(len(y_axis_num_tasks_map[i]))]
+        axs[0].plot(x_axis, y_axis_num_tasks_map[i], label=(str(label)+" GB"))
+        
+        x_axis = [i for i in range(len(y_axis_num_tasks_reduce[i]))]
+        axs[1].plot(x_axis, y_axis_num_tasks_reduce[i], label=(str(label)+" GB"))
+
+        label *= 2
+        
+    axs[0].set_ylabel('Num Tasks/Vertex - Map')
+    axs[0].set_xlabel('Vertices')
+    axs[0].legend(loc="upper left")
+
+    axs[1].set_ylabel('Num Tasks/Vertex - Reduce')
+    axs[1].set_xlabel('Vertices')
+    axs[1].legend(loc="upper left")
+
+    plt.show()    
+
+
+
+def cleanDags(dagResults):
+    minExp = dagResults[0]
+    dagHash = set()
+
+    for exp in dagResults:
+        if len(exp) < len(minExp):
+            minExp = exp
+
+    for dag in minExp:
+        dagHash.add(dag[DagProperties.index('dagDescHash')])
+
+    for exp in dagResults:
+        i = 0
+        while i < len(exp):
+            if exp[i][DagProperties.index('dagDescHash')] not in dagHash:
+                #print(exp[i][DagProperties.index('dagDescHash')])
+                del exp[i]
+            else:
+                i += 1
+
+def get_property(metrics):
+    if all((metrics[i] < metrics[i+1] for i in range(len(metrics)-1))):
+        return 1
+    if all((metrics[i] > metrics[i+1] for i in range(len(metrics)-1))):
+        return -1
+    if all((metrics[i] == metrics[i+1] for i in range(len(metrics)-1))):
+        return 0
+
+    return 2
+
+def saveToXLS(vertexResults):
+    style = xlwt.XFStyle()
+    style.num_format_str = '#,###0.00'
+    wrap_format = xlwt.XFStyle()
+    wrap_format.text_wrap = True
+
+    workbook = xlwt.Workbook()
+    worksheet1 = workbook.add_sheet('Vertex monotonicity')
+
+    column_list = zip(*vertexResults)
+    i = 0
+
+    for column in column_list:
+        for item in range(len(column)):
+            value = column[item]
+            if value == None:
+                value = 0
+            if type(value) is dict:
+                worksheet1.write(item, i, ',\n'.join('{} : {}'.format(key, val) for key, val in value.items()), style=wrap_format)
+            elif type(value) is list:
+                worksheet1.write(item, i, ',\n'.join(value), style=wrap_format)
+            elif is_number(value):
+                worksheet1.write(item, i, value, style=style)
+            else:
+                worksheet1.write(item, i, value)
+        i+=1
+
+    workbook.save('vertex_comparison.xls')
+
+    
+
+def compareVertices(vertexExpResults, dagResult):
+
+    num_exps = len(vertexExpResults)
+    num_dags = len(dagResult)
+
+    idx_dict = {}
+
+    for i in range(len(dagResult)):
+        idx_dict[dagResult[i][DagProperties.index('dagDescHash')]] = i
+
+    data = [None] * num_exps
+
+    for i in range(len(vertexExpResults)):
+        data[i] = [None] * num_dags
+        for j in range(len(vertexExpResults[i])):
+            dagHash = vertexExpResults[i][j][VertexProperties.index('dagDescHash')]
+
+            if data[i][idx_dict[dagHash]] is None:
+                data[i][idx_dict[dagHash]] = [vertexExpResults[i][j]]
+            else:
+                data[i][idx_dict[dagHash]].append(vertexExpResults[i][j])
+
+    results = [['dagId'] + ['vertexId'] + ['vertexName'] + ['CPU_util'] * num_exps + ['CPU_monotonicity'] + ['CPU_CoV'] + ['Spillage'] * num_exps + ['Spillage_monotonicity'] + ['Spillage_CoV']]
+
+    for i in range(num_dags):
+        dagId  = [dagResult[i][DagProperties.index('dagId')]]
+        dag = [dagResult[i][DagProperties.index('dagDesc')]]
+        dagHash = dagResult[i][DagProperties.index('dagDescHash')]
+        idx = idx_dict[dagHash]
+
+        vertices = data[0][idx]
+
+        for vertex in vertices:
+            cpu_utils = []
+            spillage = []
+            name = vertex[VertexProperties.index('vertexName')]
+            cpu_utils.append(vertex[VertexProperties.index('avgTaskCPUutil')])
+            spillage.append(vertex[VertexProperties.index('avgTaskSpilledRecordsPerSec')])
+            
+            flag = False
+           
+            for j in range(1, num_exps):
+                flag = False
+                for k in range(len(data[j][idx])):
+                    if name == data[j][idx][k][VertexProperties.index('vertexName')]:
+                        flag = True
+                        cpu_utils.append(data[j][idx][k][VertexProperties.index('avgTaskCPUutil')])
+                        spillage.append(data[j][idx][k][VertexProperties.index('avgTaskSpilledRecordsPerSec')])
+                        cpu_CoV = variation(cpu_utils)
+                        spillage_CoV = variation(spillage)
+
+                if not flag:
+                    break
+            if flag:
+                cpu_prop = get_property(cpu_utils)
+#                if cpu_prop is None:
+#                    continue
+                cpu_utils.append(cpu_prop)
+                cpu_utils.append(cpu_CoV)
+                spillage_prop = get_property(spillage)
+#                if spillage_prop is None:
+#                    continue
+                spillage.append(spillage_prop)
+                spillage.append(spillage_CoV)
+                result = dagId + dag + [vertex[VertexProperties.index('vertexId')]] + [name] + cpu_utils + spillage 
+                results.append(result.copy())
+    #print(results)
+
+    saveToXLS(results)
+
+
+
+def main():
+
+    workDirs = []
+
+    if len(sys.argv) < 2:
+        print("Please provide at least two dirs")
+        return 
+
+    for i in range(1, len(sys.argv)):
+        workDirs.append(sys.argv[i])
+
+    dagResults = []
+    vertexResults = []
+    filteredDagResults = []
+    filteredVertexResults = []
+
+    for i in range(len(workDirs)):
+        os.chdir(workDirs[i])
+        dagResults.append(processDags())
+        os.chdir("../")
+
+    cleanDags(dagResults)
+    cleanDags(dagResults)
+
+    for i in range(len(workDirs)):
+        os.chdir(workDirs[i])
+        vertexResults.append(processVertex(dagResults[i]))
+        filteredDagResults.append(filterDags(dagResults[i], vertexResults[i]))
+        filteredVertexResults.append(filterVertex(vertexResults[i]))
+        os.chdir("../")
+
+#    saveToXLS(dagResults, vertexResults, filteredDagResults, filteredVertexResults, startedOn)
+    compareVertices(vertexResults, dagResults[0])
+#    plotGraphDag(filteredDagResults)
+    plotGraphVertex(vertexResults)
+
+if __name__ == "__main__":
+    main()
